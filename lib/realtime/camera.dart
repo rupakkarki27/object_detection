53cbb5534c61e086b2b7d5f0fe472d9fe7ac95b2
Valmith <kvrhettlaurea@gmail.com>
2023-08-05 12:07:32 +0800

    changed threshold
    No major code changes

--- lib/realtime/camera.dart
+++ lib/realtime/camera.dart
@@ -59,7 +59,7 @@ class _CameraFeedState extends State<CameraFeed> {
               imageMean: 127.5,
               imageStd: 127.5,
               numResultsPerClass: 1,
-              threshold: 0.70,
+              threshold: 0.50,
             ).then((recognitions) {
               /*
               When setRecognitions is called here, the parameters are being passed on to the parent widget as callback. i.e. to the LiveFeed class
